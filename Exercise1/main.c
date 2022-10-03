fb9343252ddee8373e4eaf2e5b8b0eede60795ed
sashkot1 <sashkot100@gmail.com>
2022-10-03 19:39:51 +0300

    test2

---
+++ Exercise1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x=0 ,y=0,z=0, d;
+	d = x&&y;
+	d = (x&&y)||z;
+	printf("%d",d);
+
+
+	return 0;
+}
