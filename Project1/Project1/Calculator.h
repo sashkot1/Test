5ce1aebe6d7110aec1b556575bac69894bf5c59d
sashkot1 <sashkot100@gmail.com>
2022-10-03 19:03:23 +0300

    Second Test

---
+++ Project1/Project1/Calculator.h
@@ -0,0 +1,6 @@
+#pragma once
+double addition(double additive_1, double additive_2);
+double subtraction(double minuend, double sutrahend);
+double multiplication(double multiplier_1, double multiplier_2);
+double division(double divident, double divisor);
+double expondation(double base, int exponent);
