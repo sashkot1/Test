e1a34a19ad3e87df13f1a7eceb094daa3f34f007
Aleksandar <sashkot100@gmail.com>
2025-05-10 16:41:43 +0300

    First_Commit

--- Project1/Project1/Calculator.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-double addition(double additive_1, double additive_2);
-double subtraction(double minuend, double sutrahend);
-double multiplication(double multiplier_1, double multiplier_2);
-double division(double divident, double divisor);
-double expondation(double base, int exponent);
